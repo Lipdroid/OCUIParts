7e3ff01d94dced03f3a0bdee74e1253acc47d20f
Md Munir Hossain <lipuhossain67@gmail.com>
2018-02-27 10:14:32 +0600

    Initial commit

---
+++ app/src/main/java/ocuiparts/com/ocuiparts/MainActivity.java
@@ -0,0 +1,15 @@
+package ocuiparts.com.ocuiparts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static String someValue = "This is from library";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
